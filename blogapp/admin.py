f02a05905edbc562f0878a58ce41b9afaa847e0b
wish007 <326450058@qq.com>
2016-08-27 16:19:23 +0800

    add django-pagedown editor

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from blogapp.models import Article
+from pagedown.widgets import AdminPagedownWidget
+from django import forms
+
+# Define your form here.
+class ArticleForm(forms.ModelForm):
+    content = forms.CharField(widget=AdminPagedownWidget())
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+class ArticleAdmin(admin.ModelAdmin):
+    form = ArticleForm
 
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
--- myblog/settings.py
+++ myblog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blogapp',
+    'pagedown',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
