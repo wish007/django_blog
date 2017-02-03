f34618f65b8557fd6bfb169c9fa603df382669e2
wish007 <326450058@qq.com>
2017-02-03 21:58:30 +0800

    deploy to production environment

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^o^l3##ksp7c4cc5fna#du29vsai(5y41b9d%%*8olgs9j89vv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
